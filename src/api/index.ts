c3740c8217ce5b0671fdd08f93bfbf3e8b4f6342
Elias Mawa <elias@emawa.io>
2021-03-18 11:55:58 -0600

    clear repo

--- src/api/index.ts
+++
@@ -1,9 +0,0 @@
-import Authentication from "./Authentication";
-import MangaController from "./manga/MangaController";
-import UserController from "./user/UserController";
-
-export default {
-	MangaController,
-	UserController,
-	Authentication,
-};
