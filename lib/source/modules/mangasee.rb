f45ac1d418d8555bb9188647386c7e45f42d3878
Elias Mawa <elias@emawa.io>
2021-03-18 23:21:09 -0600

    create getter methods

--- lib/source/modules/mangasee.rb
+++ lib/source/modules/mangasee.rb
@@ -2,10 +2,20 @@ module Source
 	class Scraper
 		def initialize
 			@origin = 'https://mangasee123.com'
+			@name = 'mangasee123'
+			@alias = 'mangasee'
 		end
 		
 		def getOrigin
 			return @origin
+		end
+		
+		def getName
+			return @name
+		end
+		
+		def getAlias
+			return @alias
 		end
 	end
 end
