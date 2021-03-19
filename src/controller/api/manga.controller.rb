44251f446ee2e97b8960b356123be7786dce0b9d
Elias Mawa <elias@emawa.io>
2021-03-18 20:30:36 -0600

    placeholder

--- src/controller/api/manga.controller.rb
+++ src/controller/api/manga.controller.rb
@@ -1,10 +1,15 @@
+require_relative '../../lib/model/manga/chapter'
+require_relative '../../lib/model/manga/manga'
+
 module Controller
 	module Api
 		class MangaController < Grape::API
 			prefix :manga
 
 			get '/' do
-				{ Hello: 'World!' }
+				manga = Model::Source::Chapter.new(0, 'google.ca', '', '0', 'google')
+				manga.add_page 'hello world!'
+				return manga.to_json()
 			end
 		end
 	end
