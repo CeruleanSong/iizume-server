bb9832d368b30ec04f780a9c327027aa7c37a572
Elias Mawa <elias@emawa.io>
2021-03-22 03:04:45 -0600

    implement get latest from source

--- controller/api/source.controller.rb
+++ controller/api/source.controller.rb
@@ -1,30 +1,46 @@
+require "json-schema"
+
 require_relative '../../lib/source/manga/chapter'
 require_relative '../../lib/source/manga/manga'
 require_relative '../../lib/source/source_list'
+require_relative '../../lib/schema/latest'
 
 module Controller
 	module Api
 		class SourceController < Grape::API
 			prefix :source
 
-			get '/' do
+			route :any, '/' do
 				sources = $DB[:source]
 				.select(:source_id, :origin, :alias, :enabled).all
 				return sources
 			end
 
-			get '/:source' do
+			route :any, '/:source' do
 				source = $DB[:source]
 				.select(:source_id, :origin, :alias, :enabled)
-				.where(Sequel.or(name: params[:source], source_id: params[:source]))
+				.where(Sequel.or(alias: params[:source], source_id: params[:source]))
 				.first
 				return source
 			end
 			
-			get '/:source/latest' do
-				source = $DB[:manga]
-				_source = Source::SourceList::getSourceList[params[:source]]
-				return _source.getLatest
+			route :any, '/:source/latest' do
+				source = $DB[:source]
+				.select(:alias)
+				.where(Sequel.or(alias: params[:source], source_id: params[:source]))
+				.first
+
+				begin
+					req = {
+						page: params[:page] ? Integer(params[:page]) : 1,
+						limit: params[:limit] ? Integer(params[:limit]) : 30
+					}
+				rescue
+					return "invalid body?"
+				end
+
+				_source = Source::SourceList::getSource(source[:alias])
+				return _source.getLatest(req[:page], req[:limit])
 			end
 		end
 	end
