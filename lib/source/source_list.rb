540253bdf88965eef295b57cac8f6d7af2fd5826
Elias Mawa <elias@emawa.io>
2021-03-22 15:20:52 -0600

    sources are loaded and stored properly

--- lib/source/source_list.rb
+++ lib/source/source_list.rb
@@ -13,30 +13,31 @@ module Source
 			mod = `ls lib/source/modules`
 			mod.split(' ').each do|file|
 				module_name = file.split('.')[0]
-				
 				loaded_module = require_relative "modules/#{module_name}"
 
-				source_exists = $DB[:source].select(:alias, :source_id).where(alias: module_name)
-				source_id = ""
-				if source_exists.first
-					source_id = source_exists.first[:source_id]
-				else
-					source_id = SecureRandom.hex(8)
+				if loaded_module
+					source_exists = $DB[:source].select(:alias, :source_id).where(alias: module_name).first
+					source_id = source_exists ? source_exists[:source_id] : SecureRandom.hex(8)
+					
+					@@source_list[module_name] = Source::create(source_id)
+					@@source_types.push(module_name)
+
+					if !source_exists
+						$DB[:source].insert({
+							source_id: source_id,
+							origin: @@source_list[module_name].getOrigin,
+							title: @@source_list[module_name].getTitle,
+							alias: @@source_list[module_name].getAlias,
+							enabled: @@source_list[module_name].isEnabled,
+						})
+					end
 				end
 
-				@@source_list[module_name] = Source::create(source_id)
-				@@source_types.push(module_name)
-
-				if !source_exists.first
-					new_source = Model::Source.new({
-						source_id: source_id,
-						origin: @@source_list[module_name].getOrigin,
-						name: @@source_list[module_name].getName,
-						alias: @@source_list[module_name].getAlias,
-						enabled: @@source_list[module_name].isEnabled,
-					}).save
-				end
 			end
+		end
+
+		def self.getSourceTypes
+			@@source_types
 		end
 
 		def self.getSourceList
@@ -45,10 +46,6 @@ module Source
 
 		def self.getSource(_source)
 			@@source_list[_source]
-		end
-
-		def self.getSourceTypes
-			@@source_types
 		end
 	end
 end
