10d0bb32c9f44951606690a1e4750bf5f65ba23d
Elias Mawa <elias@emawa.io>
2021-03-23 00:59:52 -0600

    standardise columns

--- db/migration/008_create_page.rb
+++ db/migration/008_create_page.rb
@@ -6,7 +6,7 @@ Sequel.migration do
 			primary_key(:id, 'int(11)')
 			column(:page_id, 'varchar(16)', null: false, unique: true)
 			column(:origin, 'varchar(256)', null: false, unique: true)
-			column(:image, 'tinytext')
+			column(:image, 'varchar(256)')
 			column(:page_n, 'float(3)')
 			column :created, 'timestamp(6)', default: Sequel.function(:CURRENT_TIMESTAMP, 6)
 			column :updated, 'timestamp(6)', default: Sequel.function(:CURRENT_TIMESTAMP, 6)
